21a34bb241765b1ef915f665769805c05e610ae6
Aditya7123421 <adityaconnect2007@gmail.com>
2026-03-24 17:11:02 +0530

    Implement hover animations for Amazon cards and enable sticky lower navigation bar

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
 
 
-// const reviewCards = document.querySelectorAll(".customer-review_card");
-// reviewCards.forEach(card =>{
-//     card.addEventListener("mouseenter", ()=>{
-//         card.classList.add("customer-review_card-active");
-//     })
-//     card.addEventListener("mouseleave", ()=>{
-//         card.classList.remove("customer-review_card-active");
-//     })
-// });
+const amazonCards = document.querySelectorAll(".amazoncard");
+amazonCards.forEach(card =>{
+    const img = card.querySelector(".cardimg img");
+    const btn = card.querySelector(".imgbtn1");
+    console.log(card);
+    console.log(img);
+    card.addEventListener("mouseenter", ()=>{
+        const txt = btn.querySelector(".imgbtn1_txt");
+        img.classList.add("cardimg_hover");
+        img.classList.remove("cardimg_leave");
+        
+        txt.classList.add("change");
+        setTimeout(()=>{
+            btn.style.backgroundColor = "#e65c2e";
+            txt.innerText="Add To Cart";
+            txt.classList.remove("change");
+        },200); 
+    })
+    card.addEventListener("mouseleave", ()=>{
+        const txt = btn.querySelector(".imgbtn1_txt");
+        img.classList.remove("cardimg_hover");
+        img.classList.add("cardimg_leave");
+        
+        txt.classList.add("change");
+        setTimeout(()=>{
+            btn.style.backgroundColor = "#7FD07E";
+            txt.innerText="In Stock";
+            txt.classList.remove("change");
+        },200);
+    });
+});
